faa4c6776a469c26b89896db05e1299b35411666
Sathvika <f20240192@hyderabad.bits-pilani.ac.in>
2026-03-28 01:09:22 +0530

    Hm ok 1 is done

---
+++ Q_1a.m
@@ -0,0 +1,67 @@
+function [] = Q_1a(f,a,b,n)
+    tic;
+    h = (b-a)/n;
+    x = a:h:b;
+
+    ai = f(x);
+    bi = zeros(1,n);
+
+    for i = 1:n
+        bi(i) = (ai(i+1)-ai(i))/h;
+    end
+    toc;
+    % Plotting
+    figure;
+    hold on;
+    x_test = linspace(a,b,10000);
+    func_value = f(x_test);
+
+    y_approx = zeros(size(x_test));
+
+    for j = 1:length(x_test)
+        i = floor((x_test(j)-a)/h)+1;
+        if(i>n)
+            i=n;
+        end
+        y_approx(j) = ai(i)+bi(i)*(x_test(j)-x(i));
+    end
+  
+    plot(x_test, func_value, 'b','LineWidth',2);
+    plot(x_test,y_approx,'r--','LineWidth',2);
+
+    legend('Actual f(x)', 'Piecewise Linear Approx');
+    xlabel('x');
+    ylabel('f(x)');
+    title('Linear Piecewise Approximation');
+    grid on;
+
+    max_error = max(abs(func_value - y_approx));
+    disp(['Maximum Error = ',num2str(max_error)]);
+
+    % Calculate the error between the actual function and the approximation
+    % error = |f(x)-S(x)|
+    error = abs(func_value - y_approx);
+    figure;
+    plot(x_test, error, 'k','LineWidth',2);
+    xlabel('x');
+    ylabel('Error');
+    title('Error of Linear Piecewise Approximation');
+    grid on;
+
+
+     % Neighborhood Plots at 0
+    figure;
+    plot(x_test,func_value,'r','LineWidth',2);
+    hold on;
+    xlabel('x');
+    ylabel('f(x)')
+    plot(x_test,y_approx,'b--','LineWidth',2);
+    xlim([-0.01*h,0.01*h]);
+    ylim([1-0.01*h,1+0.01*h])
+    title(['Neighborhood near start(x = ',num2str(0),')']);
+    legend('Exact','PiecewiseLinear');
+    grid on;
+    
+
+    
+end
